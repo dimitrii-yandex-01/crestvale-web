bac43260e417c3a702d2e59ce51308d1b5a9076d
Дмитрий <russia0594@mail.ru>
2026-06-24 20:11:07 -0300

    chore: remove dist from tracking

--- dist/server/assets/_tanstack-start-manifest_v-DsgtuicT.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/joyba/Documents/CRESTVALE/web/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-C2WTY8-A.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-C2WTY8-A.js" } }] }, "/": { filePath: "C:/Users/joyba/Documents/CRESTVALE/web/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-C_03RQp0.js"] } } });
-export {
-  tsrStartManifest
-};
